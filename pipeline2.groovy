642c9cb817c6098e7611c3514c971b9ea4c011b7
monavyas082 <monika@gmail.com>
2024-01-24 23:47:17 +0530

    pipline files added

---
+++ pipeline2.groovy
@@ -0,0 +1,12 @@
+pipeline {
+     agent any
+//     stages {
+//         stage ('pull code') {
+//             agent {
+//                 label 'slave-1'
+//             }
+//             steps {
+//                 sh 'ls'
+                
+//             }
+//         }
